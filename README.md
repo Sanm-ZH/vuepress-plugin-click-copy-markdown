d0fedc4974e4564d3f295dce01adf0fa27d0703b
Sanm-ZH <zhxpvip7@gmail.com>
2021-03-26 11:07:26 +0800

    update 图标显示

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ module.exports = {
   plugins: [require('xxx/index.js'), {
     ele: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array 
     title: 'copy',
-    tips: 'click to copy'
+    tips: 'click to copy',
+    showIcon: false // boolean tips: When true, the title is not an HTML tag string, and the default icon is displayed.
   }]
 }
 ```
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const { resolve } = require('path');
 
 module.exports = (options, context) => ({
   define() {
-    const { ele, title, tips } = options;
+    const { ele, title, tips, showIcon } = options;
     return {
       COPY_ELEM: ele || ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'],
       COPY_TITLE: title || 'copy',
-      COPY_TIPS: tips || 'click to copy'
+      COPY_TIPS: tips || 'click to copy',
+      SHOW_ICON: showIcon || false
     };
   },
   name: 'vuepress-plugin-click-copy-markdown',
